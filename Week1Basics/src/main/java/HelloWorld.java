2a3f7672af4dfa880c73fa728caa50930da3e986
Ryner <individualog@gmail.com>
2024-06-28 09:58:06 +0100

    Add .git ignore and Week1Basics proj

---
+++ Week1Basics/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+
+    String message = "Hello, Java!";
+    System.out.println(message);
+    System.out.println(message.length());
+  }
+}

13229faa41084d35f3ea8120690fa8ff477060a3
Ryner <individualog@gmail.com>
2024-06-28 19:24:44 +0100

    implement greet method with empty name handling

--- Week1Basics/src/main/java/HelloWorld.java
+++ Week1Basics/src/main/java/HelloWorld.java
@@ -1,8 +1,25 @@
-public class HelloWorld {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
-    String message = "Hello, Java!";
-    System.out.println(message);
-    System.out.println(message.length());
+public class HelloWorld {
+
+  static String greet(String name) {
+    return "Hello, " + name + "!";
+
+    }
+  public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
+    String name;
+
+    do {
+      System.out.println("Please enter your name:");
+      name = userInput.nextLine().trim(); // trim() removes leading/trailing whitespace
+      if (!name.isEmpty()) {
+        System.out.println(greet(name));
+      } else {
+        System.out.println("Name cannot be empty!");
+      }
+    } while (name.isEmpty());
+
+    userInput.close();
   }
 }

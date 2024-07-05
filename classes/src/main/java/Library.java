ef12600703603ad9dbda1d4e987aa30fc812fe79
Ryner <individualog@gmail.com>
2024-07-05 16:53:00 +0100

    implemented ArrayList and streams

--- classes/src/main/java/Library.java
+++ classes/src/main/java/Library.java
@@ -1,56 +1,57 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Library {
   private final String name;
-  private Book[] collectionOfBooks;
+  private final ArrayList<Book> collectionOfBooks;
 
   public String getName() {
     return name;
   }
 
-  public Book[] getCollectionOfBooks() {
+  public ArrayList<Book> getCollectionOfBooks() {
     return collectionOfBooks;
   }
 
-  public Library(String name, Book[] collectionOfBooks) {
+  public Library(String name, ArrayList<Book>  collectionOfBooks) {
     this.name = name;
     this.collectionOfBooks = collectionOfBooks;
   }
 
   public void addBookToLibrary(Book book) {
+     collectionOfBooks.add(book);
 
-    List<Book> booksAsList = new ArrayList<>(Arrays.asList(collectionOfBooks));
-
-    try {
-      booksAsList.add(book);
-      collectionOfBooks = booksAsList.toArray(collectionOfBooks);
-    } catch (Exception e) {
-      System.out.println("Error occurred while adding a book to the library");
-    }
   }
 
+  public void displayAllBooks() {
+
+  }
+  
   public static void main(String[] args) {
 
     // Make a book
     Book exampleBook = new Book("To Kill a Mockingbird", "Harper Lee", "0001");
 
     // Make a library
-    Book[] collection = new Book[] {};
+    ArrayList<Book> collection = new ArrayList<>();
     Library exampleLibrary = new Library("Ryner's Library", collection);
-    System.out.println("collection array size before: " + collection.length);
+    System.out.println("collection array size before: " + collection.size());
 
     exampleLibrary.addBookToLibrary(exampleBook);
 
-    Book[] newCollection = exampleLibrary.getCollectionOfBooks();
-
-    System.out.println("collection array size after: " + newCollection.length);
-
-    for (int i = 0 ; i < newCollection.length; i++) {
-      Book element = newCollection[i];
-      System.out.println("Book " + (i+1) + ": {\n" + element.getInfo() + "\n}");
+    System.out.println("Method 1: ");
+    for (Book book : exampleLibrary.getCollectionOfBooks()) {
+      System.out.println(book.getInfo());
     }
+
+    System.out.println("Method 2: ");
+    collection.forEach(System.out::println);
+
+    System.out.println("Method 3: ");
+    collection.stream().map(Book::getInfo).forEach(System.out::println);
+
+    System.out.println("collection array size after: " + collection.size());
+
 
   }
 }
